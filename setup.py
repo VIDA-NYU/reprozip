47cea8f5c153acfaf86995f10b94b4a552826eb0
Remi Rampin <remirampin@gmail.com>
2014-05-06 11:42:52 -0400

    Adds BSD license & touches up setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-try:
-    from setuptools import setup, Extension
-except ImportError:
-    from distutils.core import setup, Extension
-import os
-import re
-import sys
+from setuptools import setup, Extension
 
 
 # List the source files
@@ -21,7 +15,16 @@ pytracer = Extension('reprozip._pytracer',
                      libraries=libraries)
 
 description = """
-TODO
+ReproZip is a tool aimed at scientists using Linux distributions, that
+simplifies the process of creating reproducible experiments from programs.
+
+It uses the ptrace facilities of Linux to trace the processes and files that
+are part of the experiment and build a comprehensive provenance graph for the
+user to review.
+
+Then, it can pack these files in a package to allow for easy reproducibility
+elsewhere, either by unpacking and running on a compatible machine or by
+creating a virtual machine through Vagrant.
 """
 setup(name='reprozip',
       version='0.0',
@@ -30,17 +33,17 @@ setup(name='reprozip',
       entry_points={
         'console_scripts': [
           'reprozip = reprozip.main:main']},
-      description='Reprozip -- TODO',
+      description='Linux tool enabling reproducible experiments',
       author="Remi Rampin",
       author_email='remirampin@gmail.com',
-      #url='http://github.com/remram44/reprozip',
+      url='http://github.com/remram44/reprozip',
       long_description=description,
-      license='MIT',
+      license='BSD',
       keywords=['reprozip', 'reproducibility', 'provenance'],
       classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: C',
         'Topic :: Scientific/Engineering',
