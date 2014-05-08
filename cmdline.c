f2df65c43dc1edf17a0fadfddb01846f0ffa8f7a
Remi Rampin <remirampin@gmail.com>
2014-05-08 11:33:26 -0400

    Updates cmdline.c

--- cmdline.c
+++ cmdline.c
@@ -7,7 +7,6 @@
 
 int main(int argc, char **argv)
 {
-    fprintf(stderr, "Debug mode, using database on disk\n");
     unlink("./database.sqlite3");
-    return fork_and_trace(argc - 1, argv + 1, "./database.sqlite3");
+    return fork_and_trace(argv[1], argc - 1, argv + 1, "./database.sqlite3");
 }
