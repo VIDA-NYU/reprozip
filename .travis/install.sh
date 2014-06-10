9b8d54fd9bb63cbe807defee8a948f2354edd749
Remi Rampin <remirampin@gmail.com>
2014-06-10 14:45:29 -0400

    Makes Travis get the latest rpaths from Github

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,6 +4,7 @@ case "$TEST_MODE"
 in
     run_program)
         (while read line; do echo "$line"; sh -c "$line" || exit $?; done)<<'EOF'
+        pip install 'git+https://github.com/remram44/rpaths.git#egg=rpaths'
         cd reprozip && python setup.py install
         cd reprounzip && python setup.py install
         cd reprounzip-vagrant && python setup.py install
