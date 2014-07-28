32777175a7eb7e59568d84537944d2388dec3643
Remi Rampin <remirampin@gmail.com>
2014-07-28 14:26:31 -0400

    Adds coverage reports from Travis to Coveralls

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 
+run_lines(){
+    while read line; do echo "$line"; sh -c "$line" || exit $?; done
+}
+
 case "$TEST_MODE"
 in
-    run_program)
-        (while read line; do echo "$line"; sh -c "$line" || exit $?; done)<<'EOF'
+    run_program|coverage_c)
+        if [ "$TEST_MODE" = "coverage_c" ]; then
+            export CFLAGS="-fprofile-arcs -ftest-coverage"
+        fi
+        run_lines<<'EOF'
         pip install 'git+https://github.com/remram44/rpaths.git#egg=rpaths'
+        if [ $TEST_MODE = "coverage_c" ]; then pip install cpp-coveralls; fi
         cd reprozip && python setup.py install
         cd reprounzip && python setup.py install
         cd reprounzip-vagrant && python setup.py install
+EOF
+        ;;
+    coverage_py)
+        run_lines<<'EOF'
+        pip install 'git+https://github.com/remram44/rpaths.git#egg=rpaths'
+        pip install coveralls
+        cd reprozip && python setup.py develop
+        cd reprounzip && python setup.py develop
+        cd reprounzip-vagrant && python setup.py develop
 EOF
         ;;
     check_style)
