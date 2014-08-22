2b1ed5dbefe97d19224003e3985fbfc9c6859f78
Remi Rampin <remirampin@gmail.com>
2014-08-22 14:32:58 -0400

    load_config() adds '{in,out}put_files' keys
    This allows backwards-compatibility.

--- reprounzip/reprounzip/common.py
+++ reprounzip/reprounzip/common.py
@@ -109,6 +109,13 @@ def load_config(filename, canonical, File=File, Package=Package):
     packages = read_packages(config.get('packages', []), File, Package)
     other_files = read_files(config.get('other_files', []), File)
 
+    # Adds 'input_files' and 'output_files' keys to runs
+    for run in runs:
+        if 'input_files' not in run:
+            run['input_files'] = {}
+        if 'output_files' not in run:
+            run['output_files'] = {}
+
     if canonical:
         if 'additional_patterns' in config:
             raise InvalidConfig("Canonical configuration file shouldn't have "
