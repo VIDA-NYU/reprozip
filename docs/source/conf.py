95c64ceeb0548dec88d310bfae2d493ede4a06ec
Remi Rampin <remirampin@gmail.com>
2014-10-07 13:40:38 -0400

    Adds rpaths to intersphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -265,4 +265,5 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'http://docs.python.org/': None,
+                       'http://rpaths.remram.fr/en/latest/': None}
