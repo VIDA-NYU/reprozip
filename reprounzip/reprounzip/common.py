e847e20c024b85a26d7e19a37ce63ec85c32d34f
Remi Rampin <remirampin@gmail.com>
2014-12-02 15:54:36 -0500

    Better message for unknown config version

--- reprounzip/reprounzip/common.py
+++ reprounzip/reprounzip/common.py
@@ -129,7 +129,10 @@ def load_config(filename, canonical, File=File, Package=Package):
         raise InvalidConfig("Missing version")
     # Accepts versions from 0.2 to 0.5 inclusive
     elif not LooseVersion('0.2') <= ver < LooseVersion('0.6'):
-        raise InvalidConfig("Unknown version")
+        pkgname = (__package__ or __name__).split('.', 1)[0]
+        raise InvalidConfig("Loading configuration file in unknown format %s; "
+                            "this probably means that you should upgrade "
+                            "%s" % (ver, pkgname))
     elif not keys_.issubset(set(['version', 'runs',
                                  'packages', 'other_files',
                                  'additional_patterns'])):
