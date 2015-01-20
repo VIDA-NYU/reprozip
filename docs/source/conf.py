f91b9855cc38d130ba8350290fd1ca31b4c535cb
Remi Rampin <remirampin@gmail.com>
2015-01-20 12:32:50 -0500

    Updates copyright year to 2015

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ReproZip'
-copyright = u'2014, New York University'
+copyright = u'2014-2015, New York University'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
