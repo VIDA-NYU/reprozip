71df889322045bb41619eeb81c57d440ed44286c
Remi Rampin <remirampin@gmail.com>
2015-02-01 15:29:30 -0500

    Makes allsetups.sh use pip

--- allsetups.sh
+++ allsetups.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
 set -e
+set -u
 
-cd $(dirname $0)
-DIR=$(pwd)
+cd "$(dirname $0)"
 
-cd $DIR/reprozip; python setup.py "$@"
-cd $DIR/reprounzip; python setup.py "$@"
-cd $DIR/reprounzip-docker; python setup.py "$@"
-cd $DIR/reprounzip-vagrant; python setup.py "$@"
-cd $DIR/reprounzip-vistrails; python setup.py "$@"
+PROGRAMS="./reprounzip ./reprounzip-docker ./reprounzip-vagrant ./reprounzip-vistrails"
+if [ "$(uname -s)" = Linux ]; then
+    PROGRAMS="./reprozip $PROGRAMS"
+fi
+
+arg="${1:-}"
+if [ "$arg" = install ]; then
+    pip install -U $PROGRAMS
+elif [ "$arg" = develop ]; then
+    # -e doesn't work with local paths before 6.0
+    pip install -U setuptools pip
+    for prog in $PROGRAMS; do
+        pip install -U -e $prog
+    done
+else
+    echo "Usage: $(basename "$0") <install|develop>" >&2
+    exit 1
+fi
