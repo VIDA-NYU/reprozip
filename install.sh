e6a23b423fa788a7507826472a74a656bc95e22c
Manfred Touron <m@42.am>
2015-03-27 11:42:18 +0100

    Fixed permission denied

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ sudo curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s
 sudo chmod +x /usr/local/bin/fig
 
 # Install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Download binary
