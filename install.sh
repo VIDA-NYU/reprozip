5b8a38de5c51c0aed1bc56de908ee575b735949a
William Yeh <william.pjyeh@gmail.com>
2015-04-22 16:01:46 +0800

    Update docker-compose version
    Update docker-compose version to 1.2.0.

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 set -xe
 
+# version numbers
+COMPOSE_VERSION=1.2.0
+
 
 # Disable post-install autorun
 echo exit 101 | sudo tee /usr/sbin/policy-rc.d
@@ -25,7 +28,7 @@ sudo chmod +x /usr/local/bin/fig
 
 
 # Install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 
