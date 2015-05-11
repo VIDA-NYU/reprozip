92bba0a68c373f46e4dc363b4a66571ed700ac9e
Remi Rampin <remirampin@gmail.com>
2015-05-11 01:49:49 -0400

    Install in develop mode for Python coverage

--- .travis/install.sh
+++ .travis/install.sh
@@ -24,8 +24,12 @@ in
         pip install 'git+https://github.com/remram44/usagestats.git#egg=usagestats'
         if [ $TRAVIS_PYTHON_VERSION = "2.6" ]; then pip install unittest2; fi
         if [ $TEST_MODE = "coverage_c" ]; then pip install cpp-coveralls; fi
-        if [ $TEST_MODE = "coverage_py" ]; then pip install coveralls; fi
-        pip install ./reprozip ./reprounzip ./reprounzip-docker ./reprounzip-vagrant ./reprounzip-vistrails
+        if [ $TEST_MODE = "coverage_py" ]; then
+            pip install coverage coveralls
+            pip install -e ./reprozip -e ./reprounzip -e ./reprounzip-docker -e ./reprounzip-vagrant -e ./reprounzip-vistrails
+        else
+            pip install ./reprozip ./reprounzip ./reprounzip-docker ./reprounzip-vagrant ./reprounzip-vistrails
+        fi
         ;;
     check_style)
         pip install flake8
