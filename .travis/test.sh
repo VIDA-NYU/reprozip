4c448a901225eadb8a67ea23d1cea7609cc01715
Remi Rampin <remirampin@gmail.com>
2015-05-11 01:57:16 -0400

    Update directories to test for coverage

--- .travis/test.sh
+++ .travis/test.sh
@@ -8,7 +8,9 @@ case "$TEST_MODE"
 in
     run_program|coverage_c|coverage_py)
         export PYTHONUNBUFFERED=1
-        if [ "$TEST_MODE" = "coverage_py" ]; then export COVER="coverage run --append --source=$PWD/reprozip/reprozip,$PWD/reprounzip/reprounzip,$PWD/reprounzip-vagrant/reprounzip --branch"; fi
+        if [ "$TEST_MODE" = "coverage_py" ]; then
+            export COVER="coverage run --append --source=$PWD/reprozip/reprozip,$PWD/reprounzip/reprounzip,$PWD/reprounzip-docker/reprounzip,$PWD/reprounzip-vagrant/reprounzip,$PWD/reprounzip-vistrails/reprounzip --branch"
+        fi
         if [ $TRAVIS_PYTHON_VERSION = "2.6" ]; then export REPROZIP_PYTHON=/tmp/rpz2.7/bin/python; fi
         python tests
         ;;
