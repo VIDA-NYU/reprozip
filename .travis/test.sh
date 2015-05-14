7548555b69c3caa3f5392119b8a59b31fe2a0136
Remi Rampin <remirampin@gmail.com>
2015-05-14 14:57:11 -0400

    Makes check_style and check_shared a single job

--- .travis/test.sh
+++ .travis/test.sh
@@ -18,13 +18,9 @@ in
         export COVER="coverage run --append --source=$PWD/reprozip/reprozip,$PWD/reprounzip/reprounzip,$PWD/reprounzip-vagrant/reprounzip --branch"
         python tests
         ;;
-    check_style)
+    checks)
         run_lines<<'EOF'
         flake8 --ignore=E126,E731 reprozip/reprozip reprounzip/reprounzip reprounzip-*/reprounzip
-EOF
-        ;;
-    check_shared)
-        run_lines<<'EOF'
         diff -q reprozip/reprozip/common.py reprounzip/reprounzip/common.py
         diff -q reprozip/reprozip/utils.py reprounzip/reprounzip/utils.py
 EOF
