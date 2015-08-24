5f35df66d2f3b72d431d3084e4b88e57fb77438d
Remi Rampin <remirampin@gmail.com>
2015-08-24 17:10:26 -0400

    Graph: fix possible typo losing executed files

--- reprounzip/reprounzip/unpackers/graph.py
+++ reprounzip/reprounzip/unpackers/graph.py
@@ -181,6 +181,7 @@ def generate(target, directory, all_forks=False):
             argv = tuple(r_argv.split('\0'))
             if not argv[-1]:
                 argv = argv[:-1]
+            files.add(r_name)
             edges.add((process, r_name, None, argv))
 
     process_cursor.close()
