748e0f5553c17fac275529a47c0001550b4542b2
Remi Rampin <remirampin@gmail.com>
2016-02-15 20:52:45 -0500

    Update copyright year to 2016

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ReproZip'
-copyright = u'2014-2015, New York University'
+copyright = u'2014-2016, New York University'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- reprozip/reprozip/tracer/linux_pkgs.py
+++ reprozip/reprozip/tracer/linux_pkgs.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2014-2015 New York University
+# Copyright (C) 2014-2016 New York University
 # This file is part of ReproZip which is released under the Revised BSD License
 # See file LICENSE for full license details.
 
