6ee8cd924774e211edab4817764838416d727ba5
Remi Rampin <remirampin@gmail.com>
2016-03-06 20:25:31 -0500

    Add dependency on requests

--- reprounzip/setup.py
+++ reprounzip/setup.py
@@ -14,7 +14,8 @@ with io.open('README.rst', encoding='utf-8') as fp:
 req = [
     'PyYAML',
     'rpaths>=0.8',
-    'usagestats>=0.3']
+    'usagestats>=0.3',
+    'requests']
 if sys.version_info < (2, 7):
     req.append('argparse')
 setup(name='reprounzip',
