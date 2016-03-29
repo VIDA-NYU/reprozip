6cc506518ab9a6e1209b1f6152bf48903437bd90
Remi Rampin <remirampin@gmail.com>
2016-03-29 14:35:21 -0400

    Add a YumInstaller

--- reprounzip/reprounzip/unpackers/common/packages.py
+++ reprounzip/reprounzip/unpackers/common/packages.py
@@ -86,6 +86,63 @@ class AptInstaller(object):
                                      ' '.join(pkg.name for pkg in packages))
 
 
+class YumInstaller(object):
+    """Installer for systems using RPM and Yum (Fedora, CentOS, Red-Hat).
+    """
+    @classmethod
+    def install(cls, packages, assume_yes=False):
+        options = []
+        if assume_yes:
+            options.append('-y')
+        required_pkgs = set(pkg.name for pkg in packages)
+        r = subprocess.call(['yum', 'install'] + options + list(required_pkgs))
+
+        # Checks on packages
+        pkgs_status = cls.get_packages_info(packages)
+        for pkg, status in itervalues(pkgs_status):
+            if status is not None:
+                required_pkgs.discard(pkg.name)
+        if required_pkgs:
+            logging.error("Error: some packages could not be installed:%s",
+                          ''.join("\n    %s" % pkg for pkg in required_pkgs))
+
+        return r, pkgs_status
+
+    @staticmethod
+    def get_packages_info(packages):
+        if not packages:
+            return {}
+
+        p = subprocess.Popen(['rpm', '-q'] +
+                             [pkg.name for pkg in packages] +
+                             ['--qf', '+%{NAME} %{VERSION}-%{RELEASE}\\n'],
+                             stdout=subprocess.PIPE)
+        # name -> {pkg, installed_version}
+        pkgs_dict = dict((pkg.name, (pkg, PKG_NOT_INSTALLED))
+                         for pkg in packages)
+        try:
+            for l in p.stdout:
+                if l[0] == b'+':
+                    fields = l[1:].split()
+                    if len(fields) == 2:
+                        name = fields[0].decode('ascii')
+                        status = fields[1].decode('ascii')
+                        pkg, _ = pkgs_dict[name]
+                        pkgs_dict[name] = pkg, status
+        finally:
+            p.communicate()
+
+        return pkgs_dict
+
+    @staticmethod
+    def update_script(self):
+        return ''
+
+    @staticmethod
+    def install_script(self, packages):
+        return 'yum install -y %s' % ' '.join(pkg.name for pkg in packages)
+
+
 def select_installer(pack, runs, target_distribution=THIS_DISTRIBUTION,
                      check_distrib_compat=True):
     """Selects the right package installer for a Linux distribution.
@@ -116,6 +173,10 @@ def select_installer(pack, runs, target_distribution=THIS_DISTRIBUTION,
     elif target_distribution == 'debian':
         # aptitude is not installed by default, so use apt-get here too
         installer = AptInstaller('apt-get')
+    elif (target_distribution in ('centos', 'centos linux',
+                                  'fedora', 'scientific linux') or
+            target_distribution.startswith('red hat')):
+        installer = YumInstaller()
     else:
         raise CantFindInstaller("This distribution, \"%s\", is not supported" %
                                 target_distribution.capitalize())
