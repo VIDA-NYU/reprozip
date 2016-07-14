a1a971bac8ceb7089fafd45da8b2ef5b20c8c798
Remi Rampin <remirampin@gmail.com>
2016-07-14 15:34:19 -0400

    Bump version to 1.0.7

--- reprounzip-docker/setup.py
+++ reprounzip-docker/setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.abspath(os.path.dirname(__file__)))
 with io.open('README.rst', encoding='utf-8') as fp:
     description = fp.read()
 setup(name='reprounzip-docker',
-      version='1.0.6',
+      version='1.0.7',
       packages=['reprounzip', 'reprounzip.unpackers'],
       entry_points={
           'reprounzip.unpackers': [
--- reprozip/setup.py
+++ reprozip/setup.py
@@ -42,7 +42,7 @@ req = [
     'usagestats>=0.3',
     'requests']
 setup(name='reprozip',
-      version='1.0.6',
+      version='1.0.7',
       ext_modules=[pytracer],
       packages=['reprozip', 'reprozip.tracer'],
       entry_points={'console_scripts': [
