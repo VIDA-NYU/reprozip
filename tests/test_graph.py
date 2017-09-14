c601979d5c4d7e2f8f3d50b32d5bdf7ef135057d
Remi Rampin <remirampin@gmail.com>
2017-09-14 12:39:09 -0400

    Update tests

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -180,8 +180,10 @@ other_files:
         self.do_tests(
             """\
 digraph G {
+    rankdir=LR;
+
     /* programs */
-    node [shape=box fontcolor=white fillcolor=black style=filled];
+    node [shape=box fontcolor=white fillcolor=black style="filled,rounded"];
     subgraph cluster_run0 {
         label="first run";
         prog0 [label="/bin/sh (0)"];
@@ -356,8 +358,10 @@ digraph G {
         self.do_tests(
             """\
 digraph G {
+    rankdir=LR;
+
     /* programs */
-    node [shape=box fontcolor=white fillcolor=black style=filled];
+    node [shape=box fontcolor=white fillcolor=black style="filled,rounded"];
     subgraph cluster_run0 {
         label="first run";
         prog0 [label="/bin/sh (0)"];
