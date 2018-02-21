eab37cc01f3362d89db44e73994270e6737366aa
Remi Rampin <remirampin@gmail.com>
2018-02-21 13:29:27 -0500

    Build binary wheels with manylinux2

---
+++ scripts/linux-wheels.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd "$(dirname "$0")/.."
+if [ ! -d dist ]; then mkdir dist; fi
+sudo docker run -i --rm -v "${PWD}:/src" markrwilliams/manylinux2:x86_64 <<'END'
+yum install -y sqlite-devel
+cd /src/reprozip
+for PYBIN in /opt/python/*/bin; do "${PYBIN}/pip" wheel . -w ../dist; done
+for WHEEL in ../dist/*.whl; do auditwheel repair "${WHEEL}" -w ../dist; done
+END
