06e59dec39563399979f5fdaa02cbd2e8570479d
Brian Hoffman <brianjhoffman@gmail.com>
2019-02-08 08:12:56 -0500

    adds a trace filter to gather ruby gems

--- reprozip/reprozip/filters.py
+++ reprozip/reprozip/filters.py
@@ -6,6 +6,7 @@ from __future__ import division, print_function, unicode_literals
 
 import logging
 import re
+from rpaths import Path
 
 from reprozip.tracer.trace import TracedFile
 from reprozip.utils import irange, iteritems
@@ -57,3 +58,44 @@ def python(files, input_files, **kwargs):
                 lst.append(path)
 
         input_files[i] = lst
+
+
+def ruby_gems(files, input_files, **kwargs):
+    extensions = list(map(lambda ext: ext.encode('utf-8'),
+                          ['.rb',
+                           '.haml',
+                           '.slim',
+                           '.erb',
+                           '.js',
+                           '.html']))
+    ignored_dirs = list(map(lambda ext: ext.encode('utf-8'),
+                            ['spec',
+                             'test',
+                             'tests',
+                             'guides',
+                             'doc-api',
+                             'rdoc',
+                             'doc']))
+
+    gemy_path = re.compile('.*\/ruby-[^/]*\/gems')
+    seen_paths = []
+    add = []
+
+    def consume_gem(dir_or_file):
+        gempart = Path(dir_or_file)
+        if gempart.is_file() and gempart.ext in extensions:
+            if gempart not in files:
+                logger.info("Adding %s", gempart)
+                add.append(TracedFile(gempart))
+        elif gempart.is_dir() and gempart.name not in ignored_dirs:
+            for child in gempart.listdir():
+                consume_gem(child)
+
+    for path, fi in iteritems(files):
+        m = gemy_path.match(str(path))
+        if m and m[0] not in seen_paths:
+            consume_gem(m[0])
+            seen_paths.append(m[0])
+
+    for fi in add:
+        files[fi.path] = fi
---
+++ tests/test_rails_filter.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2014-2017 New York University
+# This file is part of ReproZip which is released under the Revised BSD License
+# See file LICENSE for full license details.
+
+from __future__ import print_function, unicode_literals
+
+import sys, os
+import unittest
+import tempfile
+from rpaths import Path
+# from reprozip.tracer.trace import TracedFile
+from reprozip.common import File
+from reprozip.filters import ruby_gems
+
+class MockTracedFile(File):
+
+    def __init(self, path):
+        File.__init(self, path, None)
+
+
+class RailsFilterTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.gemdir = Path(tempfile.TemporaryDirectory(None, 'reprozip-tests').name) / 'gems/ruby-2.2.3/gems/kaminari-0.16.3'
+        cls.gemfiles = [
+            'app/views/kaminari/_first_page.html.erb',
+            'app/views/kaminari/_first_page.html.haml',
+            'app/views/kaminari/_first_page.html.slim',
+            'app/views/kaminari/_gap.html.erb',
+            'app/views/kaminari/_gap.html.haml',
+            'app/views/kaminari/_gap.html.slim',
+            'app/views/kaminari/_last_page.html.erb',
+            'app/views/kaminari/_last_page.html.haml',
+            'app/views/kaminari/_last_page.html.slim',
+        ]
+
+        for gf in cls.gemfiles:
+            gfp = cls.gemdir / gf
+            if not gfp.parent.is_dir():
+                gfp.parent.mkdir(parents=True)
+            # gfp.touch()
+            with open(str(gfp), 'a'):
+                os.utime(str(gfp), None)
+
+    def test_consuming_entire_gem(self):
+        input_files = {}
+        files = {}
+
+        for path in self.__class__.gemdir.recursedir():
+            if not path.name.find(b'_first'):
+                f = MockTracedFile(path)
+                files[f.path] = f
+
+        ruby_gems(files, input_files)
+
+        for gf in self.__class__.gemfiles:
+            gfp = self.__class__.gemdir / gf
+            self.assertIn(gfp, files.keys())
+            self.assertEqual(gfp, files[gfp].path)
