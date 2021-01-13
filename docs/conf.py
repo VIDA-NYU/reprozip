2d6bae7cc3750ffe297782369cc16ea4f6adb98c
Remi Rampin <r@remirampin.com>
2021-01-13 11:59:26 -0500

    DOC: Update link to intersphinx mapping

--- docs/conf.py
+++ docs/conf.py
@@ -340,5 +340,5 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None,
+intersphinx_mapping = {'https://docs.python.org/3/': None,
                        'http://rpaths.remram.fr/en/latest/': None}
