403356abb8f2e1bbc582e621bdd8e79991f28ddb
Remi Rampin <remi@rampin.org>
2021-04-23 16:15:43 -0400

    Add missing copyright headers

--- reprounzip-qt/reprounzip_qt/__init__.py
+++ reprounzip-qt/reprounzip_qt/__init__.py
@@ -1,1 +1,5 @@
+# Copyright (C) 2014-2017 New York University
+# This file is part of ReproZip which is released under the Revised BSD License
+# See file LICENSE for full license details.
+
 __version__ = '1.0.16'
--- reprozip-jupyter/reprozip_jupyter/__init__.py
+++ reprozip-jupyter/reprozip_jupyter/__init__.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2014-2017 New York University
+# This file is part of ReproZip which is released under the Revised BSD License
+# See file LICENSE for full license details.
+
 """Traces and packs notebook environments with ReproZip.
 """
 
