56e92c06f176bd76ae2debecfd089094e1b9fadd
Remi Rampin <remi@rampin.org>
2021-07-06 11:53:07 -0400

    Bump dependency on reprounzip
    reprounzip-docker and reprounzip-vagrant need rpztar_url().

--- reprounzip-vagrant/setup.py
+++ reprounzip-vagrant/setup.py
@@ -19,7 +19,7 @@ setup(name='reprounzip-vagrant',
               'vagrant = reprounzip.unpackers.vagrant:setup']},
       namespace_packages=['reprounzip', 'reprounzip.unpackers'],
       install_requires=[
-          'reprounzip>=1.0.10',
+          'reprounzip>=1.1',
           'rpaths>=0.8',
           'paramiko'],
       description="Allows the ReproZip unpacker to create virtual machines",
