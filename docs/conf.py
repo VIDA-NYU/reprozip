bdd6546964f4bf1b56d59955c5cdaa78e84c1935
Remi Rampin <remi@rampin.org>
2024-12-02 16:44:40 -0500

    docs: Fix intersphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -340,5 +340,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/3/': None,
-                       'http://rpaths.remram.fr/en/latest/': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'rpaths': ('http://rpaths.remram.fr/en/latest/', None),
+}
